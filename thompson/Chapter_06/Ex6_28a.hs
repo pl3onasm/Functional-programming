d53ebf5717a8d9047fdd93ba7dee40a6d8dc2b90
David <pl3onasm@gmail.com>
2025-09-08 16:50:04 +0200

    Thom 6.28a

--- thompson/Chapter_06/Ex6_28a.hs
+++ thompson/Chapter_06/Ex6_28a.hs
@@ -4,9 +4,9 @@ import qualified Data.Set as S
 -- Exercise 6.28, part 1
 
 -- | A picture is represented as a set of points
---   where each point is a (column, row) coordinate of
---   a black pixel ('#'). The background is white ('.').
---   The picture also stores its width and height.
+--   where each point is a pair (x,y) of coordinates
+--   of a black pixel. The background is assumed to
+--   be white.
 type Point = (Int, Int)
 type Width = Int
 type Height = Int
