05597de3cae47e06fa3e68d7adcad1b15e9abb62
David <pl3onasm@gmail.com>
2025-09-18 11:45:05 +0200

    updates

---
+++ thompson/Chapter_06/README.md
@@ -0,0 +1,3 @@
+# Note about chapter 6
+
+I would strongly recommend reading chapter 7.1-4 before making the exercises from chapter 6. Even though the overall idea is to apply available list functions and list comprehensions as much as possible, this is not always desirable or possible. In such cases, recursion over lists is the way to go. The mentioned sections in chapter 7 also give more information about pattern matching on lists, which will be useful in the exercises.
--- thompson/Chapter_07/Ex7_24.hs
+++ thompson/Chapter_07/Ex7_24.hs
@@ -38,10 +38,19 @@ by switching the order of the recursive calls, placing the
 larger elements first.
 
 The second modification removes duplicates by changing the
-condition y <= x to y < x in the definition of 'smaller',
-resulting in only unique elements being included in the 
-'smaller' list. This ensures that duplicate elements equal 
-to x are not included again. The result is a sorted list 
-of unique elements.
+condition y <= x to y < x in the definition of 'smaller'.
+This ensures that duplicate elements equal to x are not 
+included again. The result is a sorted list of unique 
+elements.
+
+Testing in GHCi
+
+ghci> :load Ex7_24
+ghci> qSort [7,3,-1,6,2,-7,9,8,-8,0,1,5,3,7,0,-3]
+[-8,-7,-3,-1,0,0,1,2,3,3,5,6,7,7,8,9]
+ghci> qSortDesc [7,3,-1,6,2,-7,9,8,-8,0,1,5,3,7,0,-3]
+[9,8,7,7,6,5,3,3,2,1,0,0,-1,-3,-7,-8]
+ghci> qSortNoDup [7,3,-1,6,2,-7,9,8,-8,0,1,5,3,7,0,-3]
+[-8,-7,-3,-1,0,1,2,3,5,6,7,8,9]
 
 -}
